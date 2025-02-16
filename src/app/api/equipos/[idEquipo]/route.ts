6702261d0cd684ccdeabdd997cdca87e18572d72
“Jonathan” <jonathan.esponda@estudiantes.utec.edu.uy>
2025-02-16 11:14:00 -0300

    commit

--- src/app/api/equipos/[idEquipo]/route.ts
+++ src/app/api/equipos/[idEquipo]/route.ts
@@ -4,7 +4,7 @@ import { apiFetch } from "@/app/utils/apiFetch";
 
 export async function GET(
   req: NextRequest,
-  context: { params: { idEquipo: string } }
+  { params }: { params: { idEquipo: string } }
 ) {
   // Extraer el token de la sesión usando el helper de NextAuth
   const token = await getToken({ req });
@@ -16,7 +16,7 @@ export async function GET(
   }
 
   // Obtener el id parametro de context
-  const { idEquipo } = await context.params;
+  const { idEquipo } = params;
 
   // Hacer la llamada al backend incluyendo el token en el encabezado Authorization
   const res = await apiFetch(`${process.env.API_REST}/equipos/${idEquipo}`, {
